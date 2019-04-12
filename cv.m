e110da5a9b8f71d35c667d5e5044e8daf5bb8dbb
Tutku Oztel <44998387+tutkuoztel@users.noreply.github.com>
2019-04-12 13:00:43 +0300

    Add files via upload

---
+++ cv.m
@@ -0,0 +1,27 @@
+function [val,stats] = cv(x)
+% Coefficient of variation
+%
+% This function returns the coefficient of variation value of a given vector.
+% Stats returns the std and mean.
+% Works also with column vectors.
+% class of output is scalar.
+%
+% val = cv(x)
+%
+% example: x = [1,2,3,4,5];
+% val = cv(x)
+% Written by Tutku Oztel, 03.08.2018
+
+if isvector(x) == true
+val = std(x)/mean(x);
+stats.std = std(x);
+stats.mean = mean(x);
+elseif isvector(x) == false
+    error('Shit has to be a vector.');
+elseif nargin < 1
+    error('we need more input');
+elseif nargin > 1 
+    error('we are good with less input.');
+end
+end
+
